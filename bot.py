110bdb6bc1772c71dc8f8745ee5e3ddef348d79f
Diego Beltran <diego.beltrangg24@gmail.com>
2021-07-22 11:57:01 -0700

    Se puso el procedimiento en funciones

--- bot.py
+++ bot.py
@@ -3,17 +3,77 @@ import time
 import cv2
 import pytesseract
 import numpy as np
-
+import pyautogui
 
 from colorama import Fore, Style, Back
 from time import sleep
 from PIL import *
+from tqdm import tqdm
+from getkey import getkey
 
+	
+def inicio_rutina():
+	while True:
+		key = getkey()
+		print('Presiona la tecla f para contar el score')
+		if key == 'f':
+			print('Su entrenamiento esta apunto de comenzar')
+			break	
+
+def obtencion_datos(f_copia_recibida):
+
+	f=f_copia_recibida
+	myScreenshot = pyautogui.screenshot()
+
+	myScreenshot.save(r'/home/diego/Documentos/Proyecto_aim_b/imgs/captura.png')
+
+	img = cv2.imread('imgs/captura.png')
+			
+	#Size de img_recortada_puntuacion
+	x_puntuacion=1600
+	w_puntuacion=210
+	y_puntuacion=130
+	h_puntuacion=100
+
+	#Size de img_recortada_actividad
+	x_actividad=300
+	w_actividad=500
+	y_actividad=10
+	h_actividad=100
+
+	img_recortada_puntuacion =  img[y_puntuacion:y_puntuacion+h_puntuacion, x_puntuacion:x_puntuacion+w_puntuacion]
+	img_recortada_actividad = img[y_actividad:y_actividad+h_actividad, x_actividad:x_actividad+w_actividad]
+				
+	#Se guarda las imagenes en la carpeta img
+	cv2.imwrite('imgs/captura_nueva_puntuacion.png', img_recortada_puntuacion)
+	cv2.imwrite('imgs/captura_nueva_actividad.png', img_recortada_actividad)
+	#cv2.imshow('Cropped image',img_recortada_puntuacion)
+	#cv2.imshow('Cropped image',img_recortada_actividad)
+	cv2.waitKey(0)
+				
+	#Almacenamiento de lo que la libereria pytesseract lea de las imagenes
+	puntuacion_capturada = [] 
+	puntuacion_capturada.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_puntuacion.png')))
+	actividad = []
+	actividad.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_actividad.png'),lang='eng+spa'))
+				
+
+	#print(f'Actividad: {actividad[0]}\nScore: {puntuacion_capturada[0]}')
+
+	#Aqui se obtiene la fecha local 
+	tiempo=time.localtime()
+	fecha=f'{tiempo[2]}/{tiempo[1]}/{tiempo[0]} - {tiempo[3]}:{tiempo[4]}:{tiempo[5]}'
+	#time.struct_time(tm_year=2000, tm_mon=11, tm_mday=30, tm_hour=0, tm_min=0,tm_sec=0, tm_wday=3, tm_yday=335, tm_isdst=-1)
+		        
+
+	for score in puntuacion_capturada: 
+		scores_convertido=str(score)
+		f.write(f'{fecha}\nActividad: {actividad[0]}\nScore: {puntuacion_capturada[0]}')
+
+	print(Fore.GREEN + 'Entrenamiento completado \nQue tengas un gran dia!')
 
 def run():
 	#Este es un programa que se encarga de registrar las puntuaciones de una rutina de aim
-
-
 	print(Fore.BLUE + """
 
 	Bienvenido!
@@ -23,6 +83,7 @@ def run():
     (4) Salir
 
                            """ + Style.NORMAL)
+
 	opcion = int(input('Seleccione la opcion que le interese realizar:'))
 	
 	
@@ -30,47 +91,17 @@ def run():
 
 	if opcion == 1:
 
-		img = cv2.imread('imgs/captura1.png')
-		x_puntuacion=1600
-		w_puntuacion=210
-		y_puntuacion=130
-		h_puntuacion=100
-
-		x_actividad=300
-		w_actividad=500
-		y_actividad=10
-		h_actividad=100
-
-		img_recortada_puntuacion =  img[y_puntuacion:y_puntuacion+h_puntuacion, x_puntuacion:x_puntuacion+w_puntuacion]
-		img_recortada_actividad = img[y_actividad:y_actividad+h_actividad, x_actividad:x_actividad+w_actividad]
+		duracion = int(input(Fore.BLUE + 'Escribe la duracion en minutos de tu rutina:' + Style.NORMAL))
 		
-		#img_recortada_actividad_usable = img_recortada_actividad.crop((0, 7, img_recortada_actividad.size[0], img_recortada_actividad.size[1] - 10)) 
-
-		cv2.imwrite('imgs/captura_nueva_puntuacion.png', img_recortada_puntuacion)
-		cv2.imwrite('imgs/captura_nueva_actividad.png', img_recortada_actividad)
-		cv2.imshow('Cropped image',img_recortada_puntuacion)
-		cv2.imshow('Cropped image',img_recortada_actividad)
-		cv2.waitKey(0)
-		
-		puntuacion_capturada = [] 
-		puntuacion_capturada.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_puntuacion.png')))
-		actividad = []
-		actividad.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_actividad.png'),lang='eng'))
-		
-
-		print(f'Actividad: {actividad[0]}\nScore: {puntuacion_capturada[0]}')
-
-		tiempo=time.localtime()
-		fecha=f'{tiempo[2]}/{tiempo[1]}/{tiempo[0]} - {tiempo[3]}:{tiempo[4]}:{tiempo[5]}'
-		#time.struct_time(tm_year=2000, tm_mon=11, tm_mday=30, tm_hour=0, tm_min=0,tm_sec=0, tm_wday=3, tm_yday=335, tm_isdst=-1)
-        
-		for score in puntuacion_capturada: 
-			scores_convertido=str(score)
-			texto_a_imprimir=f'{scores_convertido} {fecha} \n'
-			f.write(f'{fecha}\nActividad: {actividad[0]}\nScore: {puntuacion_capturada[0]}')
-
-		print(Fore.GREEN + 'Entrenamiento completado \nQue tengas un gran dia!')
-		
+		i=0
+		while(i < duracion):
+			#inicio_rutina()
+			
+			obtencion_datos(f)
+			print(i)
+			i += 1
+			#sleep(5)
+			
 
 	elif opcion == 2:
 		for lineas in f:
@@ -90,7 +121,7 @@ def run():
 	else:
 		print(Fore.RED + 'Ingrese una opcion valida')
 		run()
-		
+
 
 if __name__ == '__main__':
 	run()
