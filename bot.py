0b1af080fc4dfe5b8daf137d8ef5e85fa40dbbb4
Diego Beltran <diego.beltrangg24@gmail.com>
2021-07-21 19:42:35 -0700

    Falta arreglar el registro de actividades

--- bot.py
+++ bot.py
@@ -37,38 +37,40 @@ def run():
 
 		img = cv2.imread('imgs/captura.png')
 		x_puntuacion=1600
-		y_puntuacion=130
 		w_puntuacion=800
+		y_puntuacion=130
 		h_puntuacion=100
 
-		x_actividad=1600
-		y_actividad=130
-		w_actividad=800
+		x_actividad=300
+		w_actividad=450
+		y_actividad=10
 		h_actividad=100
 
 		img_recortada_puntuacion =  img[y_puntuacion:y_puntuacion+h_puntuacion, x_puntuacion:x_puntuacion+w_puntuacion]
 		img_recortada_actividad = img[y_actividad:y_actividad+h_actividad, x_actividad:x_actividad+w_actividad]
+		
+		#img_recortada_actividad_usable = img_recortada_actividad.crop((0, 7, img_recortada_actividad.size[0], img_recortada_actividad.size[1] - 10)) 
 
 		cv2.imwrite('imgs/captura_nueva_puntuacion.png', img_recortada_puntuacion)
-		cv2.imwrite('imgs/captura_nueva_actividad.png', img_recortada_puntuacion)
-		#cv2.imshow('Cropped image',img_recortada)
+		cv2.imwrite('imgs/captura_nueva_actividad.png', img_recortada_actividad)
+		cv2.imshow('Cropped image',img_recortada_actividad)
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
 
 		print(pytesseract.image_to_string(Image.open('imgs/captura_nueva_puntuacion.png')))
-		puntuacion =int([]) 
-		puntuacion.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_puntuacion.png')))
-		for scores in puntuacion:
-			puntuacion_corregida=puntuacion[scores].remover_asterisco()
-		
-		actividad = []
-		actividad.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_actividad.png')))
+		puntuacion_capturada = [] 
+		puntuacion_capturada.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_puntuacion.png')))
 
+		actividad = []
+		actividad.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_actividad.png'),lang='eng',config='--psm 8'))
+		print(actividad.append(pytesseract.image_to_string(Image.open('imgs/captura_nueva_actividad.png'))))
+	
+		
 		tiempo=time.localtime()
 		fecha=f'{tiempo[2]}/{tiempo[1]}/{tiempo[0]} - {tiempo[3]}:{tiempo[4]}:{tiempo[5]}'
 		#time.struct_time(tm_year=2000, tm_mon=11, tm_mday=30, tm_hour=0, tm_min=0,tm_sec=0, tm_wday=3, tm_yday=335, tm_isdst=-1)
                  
-		for score in puntuacion: 
+		for score in puntuacion_capturada: 
 			scores_convertido=str(score)
 			texto_a_imprimir=f'{scores_convertido} {fecha} \n'
 			f.write(f'{texto_a_imprimir}\n')
